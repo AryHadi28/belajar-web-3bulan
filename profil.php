274f56d38f7b874bd215ede678e785d6698d4dd3
Gaizka <emailgaizka@example.com>
2026-06-04 11:52:37 +0700

    feat: Tugas Halaman Profil

---
+++ profil.php
@@ -0,0 +1,28 @@
+<?php
+
+$nama = "Gaizka";
+$umur = 29;
+$kota = "Banten";
+$pekerjaan = "Programmer"
+$status = "Aktif"
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil</title>
+</head>
+<body>
+
+<h1><?= $nama ?></h1>
+
+<p>Umur: <?= $umur ?></p>
+
+<p>Kota: <?= $kota ?></p>
+
+<p>Pekerjaan: <?= $pekerjaan ?></p>
+
+<p>Status: <?= $status ?></p>
+
+</body>
+</html>
