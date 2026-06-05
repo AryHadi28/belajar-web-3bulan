20f7ca03ebe5dc9081bb0eb86307f211b615c3e3
Gaizka <emailgaizka@example.com>
2026-06-05 14:11:55 +0700

    feat: Function

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <?php
 
-$users = [
-    [
-        "nama" => "Budi",
-        "umur" => 25
-    ],
-    [
-        "nama" => "Andi",
-        "umur" => 30
-    ],
-    [
-        "nama" => "Siti",
-        "umur" => 27
-    ]
-];
-
-
-?>
-
-<ul>
-
-<?php
-foreach ($users as $user) {
-    echo $user["nama"];
+function salam()
+{
+    echo "Halo Developer";
 }
-?>
 
-</ul>
+salam();
+?>
