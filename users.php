398f26c5dba6fce5858c31e77815a4d95a939889
Gaizka <emailgaizka@example.com>
2026-07-07 14:18:03 +0700

    feat: Users

---
+++ users.php
@@ -0,0 +1,45 @@
+<?php
+
+$conn = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "belajar_php"
+);
+
+$query = "SELECT * FROM users";
+
+$result = mysqli_query(
+    $conn,
+    $query
+);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar User</title>
+</head>
+<body>
+
+<h1>Daftar User</h1>
+
+<ul>
+
+<?php while($user = mysqli_fetch_assoc($result)): ?>
+
+    <li>
+        <?= $user["nama"] ?>
+        -
+        <?= $user["email"] ?>
+        -
+        <?= $user["kota"] ?>
+    </li>
+
+<?php endwhile; ?>
+
+</ul>
+
+</body>
+</html>
